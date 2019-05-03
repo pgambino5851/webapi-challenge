e752e9483d1c328e91481ae0405edae24e0103be
pgambino5851 <pgambino5851@gmail.com>
2019-05-03 19:30:44 -0400

    Preapred to deploy to Heroku.

--- index.js
+++ index.js
@@ -14,8 +14,13 @@ Go code!
 */
 
 // code away!
+
+require('dotenv').config();
+
 const server = require('./server');
 
-server.listen(5000, () => {
-    console.log("Server running on port 5000");
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+    console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 })
